9899832ea11bd344ce9bbd27635a45411388cfc7
12imthi <110447839+12imthi@users.noreply.github.com>
2024-07-13 23:09:59 +0530

    change

--- src/assets/compounds/Api.jsx
+++ src/assets/compounds/Api.jsx
@@ -7,18 +7,18 @@ function Api() {
 
 
 
-    // const[myData,mySetData] = useState([
-    //     { id: 1, title: 'Task 1', description: 'Description 1', completed: false },
-    //     { id: 2, title: 'Task 2', description: 'Description 2', completed: true },
-    //     { id: 3, title: 'Task 3', description: 'Description 3', completed: false },
-    //     { id: 4, title: 'Task 4', description: 'Description 4', completed: true },
-    //     { id: 5, title: 'Task 5', description: 'Description 5', completed: false },
-    //     { id: 6, title: 'Task 6', description: 'Description 6', completed: true },
-    //     { id: 7, title: 'Task 7', description: 'Description 7', completed: false },
-    //     { id: 8, title: 'Task 8', description: 'Description 8', completed: true },
-    //     { id: 9, title: 'Task 9', description: 'Description 9', completed: false },
-    //     { id: 10, title: 'Task 10', description: 'Description 10', completed: true }
-    // ])
+    const[myData,mySetData] = useState([
+        { id: 1, title: 'Task 1', description: 'Description 1', completed: false },
+        { id: 2, title: 'Task 2', description: 'Description 2', completed: true },
+        { id: 3, title: 'Task 3', description: 'Description 3', completed: false },
+        { id: 4, title: 'Task 4', description: 'Description 4', completed: true },
+        { id: 5, title: 'Task 5', description: 'Description 5', completed: false },
+        { id: 6, title: 'Task 6', description: 'Description 6', completed: true },
+        { id: 7, title: 'Task 7', description: 'Description 7', completed: false },
+        { id: 8, title: 'Task 8', description: 'Description 8', completed: true },
+        { id: 9, title: 'Task 9', description: 'Description 9', completed: false },
+        { id: 10, title: 'Task 10', description: 'Description 10', completed: true }
+    ])
 
     const[data,setData] = useState([]);
     const [displayData,setDisplayData] = useState([]);
